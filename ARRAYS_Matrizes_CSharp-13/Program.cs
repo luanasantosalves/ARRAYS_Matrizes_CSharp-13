f958531f0fa066d6314b70b853c8d71c05ab3ccd
Luana <Luana@DESKTOP-ODA6GJK>
2022-04-21 22:42:51 -0300

    Adicionar arquivos de projeto.

---
+++ ARRAYS_Matrizes_CSharp-13/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ARRAYS_Matrizes_CSharp_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] nome = new string[2, 2];
+
+            for(int linha = 0; linha < 2; linha++)
+            {
+                for(int coluna = 0; coluna < 2; coluna++)
+                {
+                    nome[linha, coluna] = Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int linha = 0; linha < 2; linha++)
+            {
+                for (int coluna = 0; coluna < 2; coluna++)
+                {
+                    Console.WriteLine(nome[linha, coluna] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
